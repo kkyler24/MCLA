b35c4b38bf7ee16b49718cad71f35a486b7b073b
Jimholy <59584757+Jimholy@users.noreply.github.com>
2020-05-21 13:34:33 -0400

    fixed js server files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,9 +8,10 @@ xhr.addEventListener("readystatechange", function () {
 		console.log(this.responseText);
 	}
 });
+
 // below you can add your params after the question query = mark on line 12
 xhr.open("GET", "https://similarwords.p.rapidapi.com/moar?query=cat");
 xhr.setRequestHeader("x-rapidapi-host", "similarwords.p.rapidapi.com");
 xhr.setRequestHeader("x-rapidapi-key", "aaea0493d3msh31fddf1759e8b84p100a64jsnf53a382e57c0");
-
 xhr.send(data);
+
--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(express.static("public"));
 // should be inside html-routes.js
 
 app.get("/", function(req, res) {
+  
   res.sendFile(path.join(__dirname,"public/index.html"))
 });
 
@@ -58,7 +59,7 @@ app.get("/about", function(req, res) {
   res.sendFile(path.join(__dirname,"public/about.html"))
 });
 
-db.sequelize.sync({}).then(function() {
+db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
